b82fc298bf98d7f8001561e52a4e55ae5e8ce403
Luca <s4493578@studenti.unige.it>
2021-02-09 12:01:43 +0100

    L'inizio degli inizi

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def import_csv():
+    import_fields = pd.read_csv('redacted-2020-june-30-wprdc-.csv', header=None).to_numpy()[0, :]
+    import_values = pd.read_csv('redacted-2020-june-30-wprdc-.csv').to_numpy()
+    return import_fields, import_values
+
+
+def remove_ei(remove_fields: np.ndarray, remove_values: np.ndarray):
+    remove_fields = remove_fields[2:10]
+    remove_values = remove_values[:, 2:10]
+    return remove_fields, remove_values
+
 
 if __name__ == '__main__':
-    fields = pd.read_csv('redacted-2020-june-30-wprdc-.csv', header=None).to_numpy()[0, :]
-    values = pd.read_csv('redacted-2020-june-30-wprdc-.csv').to_numpy()
-    print(fields)
-    print(values[0])
+    [fields, values] = import_csv()
+    print(list(fields))
+    [fields, values] = remove_ei(fields, values)
+    print(list(fields))
