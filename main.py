86ebcb68cd0977b54cad764c087b966f9cfbe3b4
Luca <s4493578@studenti.unige.it>
2021-02-09 11:33:33 +0100

    L'inizio degli inizi

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 
 if __name__ == '__main__':
-    fields = pd.read_csv('redacted-2020-june-30-wprdc-.csv', sep=',',).to_numpy()[0, :]
-    values = pd.read_csv('redacted-2020-june-30-wprdc-.csv', sep=',', header=None).to_numpy()
-    print(fields[0])
+    fields = pd.read_csv('redacted-2020-june-30-wprdc-.csv', header=None).to_numpy()[0, :]
+    values = pd.read_csv('redacted-2020-june-30-wprdc-.csv').to_numpy()
+    print(fields)
+    print(values[0])
